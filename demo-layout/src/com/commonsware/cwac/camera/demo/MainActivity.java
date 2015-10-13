1092d158cffed3d88dcc63fe676c59a1a71d7df9
Paul Peavyhouse <pv@swooby.com>
2015-10-12 23:24:43 -0700

    demo-layout on API23 that throws "LinkageError: DemoCameraFragment"

--- demo-layout/src/com/commonsware/cwac/camera/demo/MainActivity.java
+++ demo-layout/src/com/commonsware/cwac/camera/demo/MainActivity.java
@@ -14,14 +14,16 @@
 
 package com.commonsware.cwac.camera.demo;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 import com.commonsware.cwac.camera.CameraHost;
 import com.commonsware.cwac.camera.CameraHostProvider;
 import com.commonsware.cwac.camera.SimpleCameraHost;
 
-public class MainActivity extends Activity implements
-    CameraHostProvider {
+public class MainActivity
+        extends AppCompatActivity
+        implements CameraHostProvider {
   private DemoCameraFragment current=null;
 
   @Override
