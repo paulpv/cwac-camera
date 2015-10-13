be9c64abc7cf78a394d63d8caf67cbadac92db75
Paul Peavyhouse <pv@swooby.com>
2015-10-13 11:35:06 -0700

    Manually matching some code w/ origin/master

--- demo-layout/src/com/commonsware/cwac/camera/demo/MainActivity.java
+++ demo-layout/src/com/commonsware/cwac/camera/demo/MainActivity.java
@@ -1,15 +1,14 @@
 /***
- * Copyright (c) 2013 CommonsWare, LLC
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain
- * a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+  Copyright (c) 2013 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may
+  not use this file except in compliance with the License. You may obtain
+  a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
  */
 
 package com.commonsware.cwac.camera.demo;
@@ -21,11 +20,9 @@ import com.commonsware.cwac.camera.CameraHost;
 import com.commonsware.cwac.camera.CameraHostProvider;
 import com.commonsware.cwac.camera.SimpleCameraHost;
 
-public class MainActivity
-        //extends AppCompatActivity
-        extends Activity
-        implements CameraHostProvider {
-    private DemoCameraFragment current = null;
+public class MainActivity extends Activity implements
+        CameraHostProvider {
+    private DemoCameraFragment current=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +30,7 @@ public class MainActivity
 
         setContentView(R.layout.activity_main);
 
-        current = new DemoCameraFragment();
+        current=new DemoCameraFragment();
 
         getFragmentManager().beginTransaction()
                 .replace(R.id.container, current).commit();
